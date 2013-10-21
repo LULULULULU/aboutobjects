24241cfd9258d5d71bdccf222419a554d6344ca7
Lu Wu <lu.wu@cerner.com>
2013-10-21 10:19:27 -0500

    Initial Commit

---
+++ aboutobjects/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  aboutobjects
+//
+//  Created by Wu,Lu on 10/21/13.
+//  Copyright (c) 2013 Wu,Lu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
